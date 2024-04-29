8dc4c5e1e5322d8b0b64d3f7c355274bea9102b3
Hayato <hayato.314.nico@gmail.com>
2024-04-29 16:09:44 +0900

    配置は上位のオブジェクトのプロパティで設定するイメージ

--- lib/main.dart
+++ lib/main.dart
@@ -45,25 +45,29 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: <Widget>[
-          Container(
-            color:Colors.blue,
-            width: 200,
-            height: 200,
-          ),
-          Container(
-            color:Colors.yellow,
-            width: 150,
-            height: 150,
-          ),
-          Container(
-            color:Colors.red,
-            width: 100,
-            height: 100,
-          ),
-        ],
+      body: Align(
+        alignment: Alignment.bottomRight,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: <Widget>[
+            Container(
+              color:Colors.blue,
+              width: 200,
+              height: 200,
+            ),
+            Container(
+              color:Colors.yellow,
+              width: 150,
+              height: 150,
+            ),
+            Container(
+              color:Colors.red,
+              width: 100,
+              height: 100,
+            ),
+          ],
+        ),
       ),
     );
   }
